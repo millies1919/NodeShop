4dbc464d9669c52f0c3f85c718c26b1f2197352d
Matt Illies <mattillies19@gmail.com>
2020-03-31 19:53:38 -0500

    adding in templates

--- Node Projects/Express1/routes/admin.js
+++ Node Projects/Express1/routes/admin.js
@@ -6,14 +6,20 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res) => {
   res.setHeader('Content-Type', 'text/html');
-  res.sendFile(path.join(rootDir, '..', 'views', 'add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product'
+  });
 });
 
 router.post('/add-product', (req, res) => {
-  console.log(req.body);
+  this.products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
