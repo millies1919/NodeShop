faa606957713fbf519d2331f046c6b579fd9db0b
Matt Illies <mattillies19@gmail.com>
2020-04-08 00:34:30 -0500

    added MVC layout

---
+++ Node Projects/Express1/controllers/products.js
@@ -0,0 +1,30 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+};
+
+exports.postAddProducts = (req, res) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
+
+exports.getProduct = (req, res) => {
+  const products = Product.fetchAll();
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
+};
