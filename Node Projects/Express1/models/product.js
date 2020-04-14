6f3a5317aa51f365cb908e088d4d595146f5f1c6
Matt Illies <mattillies19@gmail.com>
2020-04-13 23:56:14 -0500

    added in some views

--- Node Projects/Express1/models/product.js
+++ Node Projects/Express1/models/product.js
@@ -1,36 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
-const productsFilePath = path.join(
+const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
   'products.json'
 );
 
-const getProductsFromFile = (callBack) => {
-  fs.readFile(productsFilePath, (error, fileData) => {
-    if (error) {
-      return callBack([]);
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    callBack(JSON.parse(fileData));
   });
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(productsFilePath, JSON.stringify(products), (error) => {
-        console.log(error);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
       });
     });
   }
 
-  static fetchAll(callBack) {
-    getProductsFromFile(callBack);
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
