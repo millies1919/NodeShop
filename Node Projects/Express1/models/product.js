fbf2ad48caa1b16d6db18978161a392dcd37b419
Matt Illies <mattillies19@gmail.com>
2020-04-16 14:26:05 -0500

    small refactor

--- Node Projects/Express1/models/product.js
+++ Node Projects/Express1/models/product.js
@@ -7,7 +7,7 @@ const p = path.join(
   'products.json'
 );
 
-const getProductsFromFile = cb => {
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -26,9 +26,10 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile(products => {
+    this.id = Math.random().toString();
+    getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
+      fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
@@ -36,5 +37,14 @@ module.exports = class Product {
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static finfById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((product) => {
+        return product.id === id;
+      });
+      CDATASection(product);
+    });
   }
 };
